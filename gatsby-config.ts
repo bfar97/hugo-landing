eb9904c03345a9d8e805765d5b423fe4e1dcb285
Bernardo Rodrigues <bfar97@gmail.com>
2023-10-21 15:13:07 +0100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,28 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `hugoaraujo`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "1p2ubesp6SDkf5Kpq6CuiQslFv6aku84S3AuNeO31C0",
+      "spaceId": "klq8thtq9tax"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
+
+export default config;
